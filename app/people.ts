8a3b76014bd391f522cc731e4f0e089ebcb503de
Davi Ferreira de Lima <daviferreiradelima.contato@gmail.com>
2020-02-08 00:48:34 +0000

    Adicionando classe People com alguns atributos básicos.

--- app/people.ts
+++ app/people.ts
@@ -1,5 +1,16 @@
 namespace enterprise {
-    class People() {
-        
+    class People {
+        // Criação de atributo com conceito de union types.
+        private name:string|undefined;
+        private cpf:string|undefined;
+        private age:number|undefined;
+
+        getName():string|undefined{
+            return this.name;
+        }
+
+        setName(name:string):void{
+            this.name = name;
+        }
     }
 }
