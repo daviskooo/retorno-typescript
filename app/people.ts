9edb1bbdfb500a512c2a6709f4728bbb37ee5b88
Davi Ferreira de Lima <daviferreiradelima.contato@gmail.com>
2020-02-08 01:05:20 +0000

    Adicionando construtor com regra para CPF.

--- app/people.ts
+++ app/people.ts
@@ -2,8 +2,13 @@ namespace enterprise {
     class People {
         // Criação de atributo com conceito de union types.
         private name:string|undefined;
-        private cpf:string|undefined;
+        private cpf:string;
         private age:number|undefined;
+
+        // Quando a classe for construída, é obrigatório que haja o valor de CPF. Assim pode-se retirar o union type do cpf.
+        constructor(cpf:string){
+            this.cpf = cpf;
+        }
 
         getName():string|undefined{
             return this.name;
@@ -19,6 +24,11 @@ namespace enterprise {
 
         setAge(age:number):void{
             this.age = age;
+        }
+
+        // Criando apenas um get para o CPF pois na regra não será possível alterar o valor deste.
+        getCpf():string{
+            return this.cpf;
         }
     }
 }
