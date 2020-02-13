3e09d834df57157361dce1068ca169a1f31ab4ec
Davi Ferreira de Lima <daviferreiradelima.contato@gmail.com>
2020-02-13 01:44:47 +0000

    Adicionando a mostra dos outros atributos no HTML.

---
+++ app/implement.js
@@ -0,0 +1,10 @@
+"use strict";
+var enterprise;
+(function (enterprise) {
+    var p = new enterprise.People("Davi", "123.456.789-00");
+    p.setAge(19);
+    // O TS vai indicar que o name possívelmente pode ser null.
+    document.getElementById("name").textContent = p.getName();
+    document.getElementById("cpf").textContent = p.getCpf();
+    document.getElementById("age").textContent = p.getAge();
+})(enterprise || (enterprise = {}));
